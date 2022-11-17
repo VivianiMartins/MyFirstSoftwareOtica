6c5ffcde188de6bc24a1047917c4af010645a0df
Leandro Rodrigues <71800377+Leandro1210@users.noreply.github.com>
2022-11-17 00:06:02 -0300

    Add files via upload
    Adição das seguintes telas: TelaLogin, TelaNovoCliente, TelaEstoqueDisponivelArmacao, TelaEstoqueDisponivelLente, TelaNovoCliente eTelaPesquisa.

---
+++ src/view/TelaLogin.java
@@ -0,0 +1,163 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JToolBar;
+import controller.LoginController;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+
+public class TelaLogin extends JFrame implements ActionListener{
+	private ActionListener controller;
+
+	private JFrame frame;
+	private JPanel panel;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JLabel lblNewLabel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin window = new TelaLogin();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+
+	/**
+	 * Create the application.
+	 */
+	public TelaLogin() {
+		this.controller = controller;
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 825, 680);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		panel = new JPanel();
+		panel.setBackground(new Color(200, 50, 43));
+		panel.setForeground(new Color(200, 50, 43));
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+
+		JPanel panel_1 = new JPanel();
+
+		JButton btnNewButton = new JButton("Entrar");
+		btnNewButton.setFont(new Font("Roboto", Font.PLAIN, 24));
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(200, 50, 43));
+		btnNewButton.setContentAreaFilled(false);
+	    btnNewButton.setOpaque(true);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {}
+			
+		});
+
+		lblNewLabel = new JLabel("");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\leand\\Downloads\\logo_transparente.png"));
+
+		textField_1 = new JTextField();
+		textField_1.setBackground(new Color(229, 229, 229));
+		textField_1.setColumns(10);
+
+		JLabel lblNewLabel_1_1 = new JLabel("Senha");
+		lblNewLabel_1_1.setFont(new Font("Roboto", Font.PLAIN, 24));
+
+		textField = new JTextField();
+		textField.setBackground(new Color(229, 229, 229));
+		textField.setColumns(10);
+
+		JLabel lblNewLabel_1 = new JLabel("Login");
+		lblNewLabel_1.setFont(new Font("Roboto", Font.PLAIN, 24));
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(121)
+					.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(135))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(24)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 565, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(52, Short.MAX_VALUE))
+		);
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(79)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
+						.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE))
+					.addGap(40))
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(207)
+					.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE)
+					.addGap(213))
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(57)
+					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(32))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(36)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 183, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_1)
+					.addGap(11)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+					.addGap(27)
+					.addComponent(lblNewLabel_1_1)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+					.addGap(35)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+					.addGap(73))
+		);
+		panel_1.setLayout(gl_panel_1);
+		panel.setLayout(gl_panel);
+	}
+
+
+//isso tem ue vir de outro lugr
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
