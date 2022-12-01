0745afa5057a63774b7f4c099fd3381c12112cde
Leandro Rodrigues <71800377+Leandro1210@users.noreply.github.com>
2022-11-30 21:26:56 -0300

    Adição da tela ordem de serviço

---
+++ estoque/model/Armacao.java
@@ -0,0 +1,73 @@
+package estoque.model;
+
+public class Armacao extends Produto{
+	
+	private String marca;
+	private int cor;
+	private String nomeModelo;
+	private String codPreco;
+	
+	public Armacao (){
+		this.setMarca(null);
+		this.setCor(0);
+		this.setNomeModelo(null);
+		this.setCodPreco(null);
+	}
+	
+	public Armacao(String tipo, double precoDeCusto, double precoDeVenda, int quantidade, String marca, int cor, String nomeModelo, String codPreco) {
+		super(tipo, precoDeCusto, precoDeVenda, quantidade);
+		this.setMarca(marca);
+		this.setCor(cor);
+		this.setNomeModelo(nomeModelo);
+		this.setCodPreco(codPreco);
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getCor() {
+		return cor;
+	}
+
+	public void setCor(int cor) {
+		this.cor = cor;
+	}
+
+	public String getNomeModelo() {
+		return nomeModelo;
+	}
+
+	public void setNomeModelo(String nomeModelo) {
+		this.nomeModelo = nomeModelo;
+	}
+
+	public String getCodPreco() {
+		return codPreco;
+	}
+
+	public void setCodPreco(String codPreco) {
+		this.codPreco = codPreco;
+	}
+
+	public void criarArmacao() {
+
+	}
+
+	public void editarArmacao() {
+
+	}
+	public void pesquisarArmacao() {
+
+	}
+	
+	public String toString() {
+		String texto = "\nMarca: "+this.getMarca()+"\nCor: "+this.getCor()+"\nNome do modelo: "+this.getNomeModelo()
+		+"\nCodigo preco: "+this.getCodPreco();
+		return texto;
+	}
+}
---
+++ estoque/model/Produto.java
@@ -0,0 +1,77 @@
+package estoque.model;
+
+public class Produto {
+
+	private String tipo;
+	private double precoDeCusto;
+	private double precoDeVenda;
+	private int quantidade;
+	
+	public Produto() {
+		this.setTipo(null);
+		this.setPrecoDeCusto(0);
+		this.setPrecoDeVenda(0);
+		this.setQuantidade(0);
+	}
+	
+	public Produto(String tipo, double precoDeCusto, double precoDeVenda, int quantidade) {
+		this.setPrecoDeCusto(precoDeCusto);
+		this.setTipo(tipo);
+		this.setPrecoDeVenda(precoDeVenda);
+		this.setQuantidade(quantidade);
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public float getPrecoDeCusto() {
+		return (float) precoDeCusto;
+	}
+
+	public void setPrecoDeCusto(double precoDeCusto) {
+		this.precoDeCusto = precoDeCusto;
+	}
+
+	public float getPrecoDeVenda() {
+		return (float) precoDeVenda;
+	}
+
+	public void setPrecoDeVenda(double precoDeVenda) {
+		this.precoDeVenda = precoDeVenda;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public void criarProduto() {
+
+	}
+
+	public void editarProduto() {
+
+	}
+
+	public void pesquisarProduto() {
+
+	}
+
+	public void excluirProduto() {
+
+	}
+	
+	public String toString() {
+		String texto = "Tipo: "+this.getTipo()+"Preco de custo: "+this.getPrecoDeCusto()+"Preco de venda: "+this.getPrecoDeVenda()
+		+"Quantidade: "+this.getQuantidade();
+		return texto;
+	}
+}
